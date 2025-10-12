1f94df30a8f9d74e56f81548a1f7800724fe2c9a
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2025-10-13 06:58:18 +0800

    buat layout

---
+++ src/layouts/menu.ts
@@ -0,0 +1,25 @@
+export interface MenuItem {
+  label: string;
+  href?: string;
+  icon?: string;
+  submenus?: MenuItem[];
+}
+
+export const menus = [
+  { label: "Dashboard", href: "/dashboard", icon: "lucide:area-chart" },
+  {
+    label: "Sekretariat",
+    submenus: [
+      {
+        href: "/sekretariat/inventaris-kantor",
+        label: "Inventaris Kantor",
+        icon: "lucide:package",
+      },
+      {
+        href: "/sekretariat/kepegawaian",
+        label: "Kepegawaian",
+        icon: "lucide:users",
+      },
+    ],
+  },
+] satisfies MenuItem[];
